51e94d6587136bdfea8db4511801665907f39c8d
andrewefish <aefish@bu.edu>
2022-06-23 13:50:22 -0400

    final

---
+++ script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+                data: {
+                    activities: [
+                        {activity: "Men's Club Basketball"},
+                        {activity: "Freshman Orientation"},
+                        {activity: "Men's Club Baseball"},
+                        {activity: "Computer Science Tutor"},
+                        {activity: "Intramural Sports"},
+                        {activity: "Study Abroad: Barcelona"}
+                        
+                    ]
+
+                }
+
+})
